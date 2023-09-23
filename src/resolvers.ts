4e65c4f186769a6e285f420108612b1c78ebfde4
nenadV91 <nenadwebdevelop@gmail.com>
2023-09-23 15:54:56 +0200

    feat: update

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,9 +1,65 @@
-import { books } from "./data";
+import * as data from "./data";
 
 // Resolvers define how to fetch the types defined in your schema.
 // This resolver retrieves books from the "books" array above.
+
+let { authors, games, reviews } = data;
+
+const genRand = () => Math.floor(Math.random() * 1000000);
+
 export const resolvers = {
   Query: {
-    books: () => books,
+    games: () => games,
+    authors: () => authors,
+    reviews: () => reviews,
+    review: (_, args) => {
+      return reviews.find(({ id }) => id == args.id);
+    },
+    author: (_, args) => {
+      return authors.find(({ id }) => id == args.id);
+    },
+    game: (_, args) => {
+      return games.find(({ id }) => id == args.id);
+    },
+  },
+  Game: {
+    reviews: (parent) => {
+      return reviews.filter(({ game_id }) => game_id === parent.id);
+    },
+  },
+  Author: {
+    reviews: (parent) => {
+      return reviews.filter(({ game_id }) => game_id === parent.id);
+    },
+  },
+  Review: {
+    game: (parent) => {
+      return games.find(({ id }) => id === parent.game_id);
+    },
+    author: (parent) => {
+      return authors.find(({ id }) => id === parent.author_id);
+    },
+  },
+  Mutation: {
+    addGame: (_, args) => {
+      const game = { ...args.game, id: genRand() };
+      games.push(game);
+      return game;
+    },
+    deleteGame: (_, args) => {
+      games = games.filter(({ id }) => id != args.id);
+      return games;
+    },
+    updateGame: (_, args) => {
+      games = games.map((game) => {
+        if (game.id === args.id) {
+          return { ...game, ...args.edits };
+        }
+
+        return game;
+      });
+
+      return games.find(({ id }) => id === args.id);
+    },
   },
 };
--- src/schema.ts
+++ src/schema.ts
@@ -1,29 +1,48 @@
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
 export const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: Author
+  type Game {
+    id: ID!
+    title: String!
+    platform: [String!]!
+    reviews: [Review!]
   }
-
+  
   type Author {
-    name: String
-    books: [Book]
+    id: ID!
+    name: String!
+    verified: Boolean!
+    reviews: [Review!]
   }
 
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Review {
+    id: ID!
+    rating: Int!
+    content: String!
+    game: Game!
+    author: Author!
+  }
+
   type Query {
-    books: [Book]
+    reviews: [Review]
+    review(id: ID!): Review
+    games: [Game]
+    game(id: ID!): Game
     authors: [Author]
+    author(id: ID!): Author
   }
 
   type Mutation {
-    addBook(title: String, author: String): Book
+    addGame(game: AddGameInput!): Game
+    deleteGame(id: ID!): [Game]
+    updateGame(id: ID!, edits: EditGameInput!): Game
+  }
+
+  input AddGameInput {
+    title: String!
+    platform: [String!]!
+  }
+
+  input EditGameInput {
+    title: String
+    platform: [String!]
   }
 `;
